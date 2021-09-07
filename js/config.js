670f17ecf6365e148362121948409192b2c7923b
hiroyuki-kirihara <hiroyuki.kirihara.8bloom@gmail.com>
2021-09-07 11:36:17 +0900

    Edit config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_frchm8fA0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4b0od2e8sjp8l017kvi916a8ek', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
